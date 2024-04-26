bc2aee534f9ba580c28bf6803198ecc098ac3056
FouadALLAOUI <allaoui70fouad71@gmail.com>
2024-04-26 14:46:42 +0100

    Create the entity fot Resource

---
+++ src/main/java/com/riodan/SpringBootSpringDataJPA/models/Video.java
@@ -0,0 +1,17 @@
+package com.riodan.SpringBootSpringDataJPA.models;
+
+import jakarta.persistence.Entity;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Entity
+public class Video extends Resource {
+
+    private int length;
+
+}
