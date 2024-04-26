c9a068eabfe72beea7549728a770a73996929c22
FouadALLAOUI <allaoui70fouad71@gmail.com>
2024-04-26 10:24:28 +0100

    Create the project

---
+++ src/main/java/com/riodan/SpringBootSpringDataJPA/SpringBootSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.riodan.SpringBootSpringDataJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSpringDataJpaApplication.class, args);
+	}
+
+}
